67720695f1c8cd7df8adafd70e2cf672f49cb6e8
Mello0324 <mamellokhokoane@gmail.com>
2024-04-17 18:59:02 +0200

    changed weather forecast emoji

--- src/index.js
+++ src/index.js
@@ -63,7 +63,7 @@ function displayForecast() {
     `
     <div class= "weather-forecast-day">
     <div class = "weather-forecast-date">${day}</div>
-    <div class = "weather-forecast-icon">🌥</div>
+    <div class = "weather-forecast-icon">🌧</div>
     <div class="weather-forecast-temperatures">
     <div class="weather-forecast-temperature">
      <strong>15°C</strong>
