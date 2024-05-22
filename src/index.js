26eae4ade0f9387e4e8c224db50a8d9076a45302
Mello0324 <mamellokhokoane@gmail.com>
2024-05-22 11:44:55 +0200

    Updated index.js

--- src/index.js
+++ src/index.js
@@ -52,6 +52,13 @@ function handleSearchSubmit(event) {
   searchCity(searchInput.value);
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[date.getDay()];
+}
+
 function getForecast(city) {
   let apiKey = "8db2acdt108befcaed4d3o9902ea6bf1";
   let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
@@ -59,8 +66,6 @@ function getForecast(city) {
 }
 
 function displayForecast(response) {
-  console.log(response.data);
-
   let forecastHtml = "";
 
   response.data.daily.forEach(function (day, index) {
